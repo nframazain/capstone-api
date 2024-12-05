67a093e9279eae3bbd6fe659d98bc6d68d1434e4
Wuzi19 <zainilmi097@gmail.com>
2024-12-06 01:55:02 +0700

    Baru nambahin dikit dah ngantuk gw

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,5 @@
 const { nanoid } = require('nanoid')
-const user
+const users = require(/*dari db*/)
 
 const addUserHandler = (request, h) => {
     const {
@@ -11,5 +11,40 @@ const addUserHandler = (request, h) => {
         organizationName
     } = request.payload;
 
-    const
-}
+    if (role != "donor" && role != "recipient") {
+        const response = h.respones({
+            status: "fail",
+            message: "Invalid role. Role must be filled",
+        });
+        response.code(400);
+        return response;
+    }
+
+    const userId = nanoid(16);
+    
+    const newUser = {
+        userId, 
+        name, 
+        email, 
+        phone, 
+        role
+    };
+
+    users.push(newUser);
+
+    if(isSuccess) {
+        const response = h.response({
+            status: 'success',
+            message: 'User successfully added',
+        })
+        response.code(201);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Unable to add new user',
+    })
+    response.code(500);
+    return response; 
+};
