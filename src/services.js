52453f65b2ba6b2a6229752adb03b51e99324374
Naswan Fachri Ramadhan Zain <168632002+nframazain@users.noreply.github.com>
2024-12-09 17:14:34 +0700

    Update services.js

--- src/services.js
+++ src/services.js
@@ -1,15 +1,1 @@
-// const admin = require("firebase-admin");
-// const { getFirestore } = require("firebase-admin/firestore");
 
-// var serviceAccount = require("./serviceAccountKey.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL:
-//     "https://centering-abode-438216-q7-default-rtdb.asia-southeast1.firebasedatabase.app",
-// });
-
-// //untuk Firestore
-// const db = getFirestore();
-
-// module.exports = { db, admin };
